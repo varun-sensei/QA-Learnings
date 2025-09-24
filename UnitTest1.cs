207cd45d52c92e9c65be7de6df14179496dfcde4
varun-sensei <wakeuptoreality160@gmail.com>
2025-09-24 15:30:59 +0530

    Initial commit

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SeleniumCSharpFramework
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
